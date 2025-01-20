2f2ae23fdbebfa58c76979b2ba2f5a6e38cfae63
jhyeong <hyeongcode@gmail.com>
2025-01-20 16:58:20 +0900

    feat: models/WorkLogs add

--- models/WorkLogs.js
+++ models/WorkLogs.js
@@ -1,6 +1,9 @@
 import {sequelize} from "../config/db.js";
 import {DataTypes} from "sequelize";
+import Employer from "./Employer.js";
+import PartTimer from "./PartTimer.js";
 
+//근태
 const WorkLogs = sequelize.define('WorkLogs', {
 
     wlno: { //Primary Key
@@ -9,7 +12,34 @@ const WorkLogs = sequelize.define('WorkLogs', {
         primaryKey: true,
         autoIncrement: true
     },
+    wlstartTime: {  //출근 시간
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    wlendTime: {    //퇴근 시간
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    wlworkStatus: { //근태 상태(0: 정상 출근, 1: 지각, 2: 조퇴, 3: 결근)
+        type: DataTypes.TINYINT,
+        allowNull: true
+    },
+    wlregdate: {    //테이블 생성 시간
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    wlchangedStartTime: {   //변경 된 출근 시간
+        type: DataTypes.TIME,
+        allowNull: true
+    },
+    wlchangedEndTime: { //변경 된 ㅗ티근 시간
+        type: DataTypes.TIME,
+        allowNull: true
+    }
+});
 
-})
+WorkLogs.belongsTo(Employer, {foreignKey: 'eno'});
+WorkLogs.belongsTo(PartTimer, {foreignKey: 'pno'});
 
 export default WorkLogs;
