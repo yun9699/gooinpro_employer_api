f5b1d3175a98bb3fc3c2b2d017d7e244c4373ae0
jhyeong <hyeongcode@gmail.com>
2025-01-20 15:49:50 +0900

    feat: db conect

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require('express');
+import express from 'express';
+import Employer from './models/Employer.js';
+
 const app = express();
 const PORT = 3000;
+
+// '/employers' 경로로 Employer 데이터 조회
+app.get('/employers', async (req, res) => {
+    try {
+        const employers = await Employer.findAll();
+        res.json(employers);  // 조회된 데이터를 JSON 형식으로 반환
+    } catch (err) {
+        console.error('Employer 조회 실패:', err);
+        res.status(500).send('서버 오류');
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, Node.js!');
