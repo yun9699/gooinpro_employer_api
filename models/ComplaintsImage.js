9a79fba0efeb9fa631ab35770a3ddc4b4eeb4d8a
EonEon2 <duqdjs123@naver.com>
2025-01-20 17:01:48 +0900

    feat: complaintsimage

--- models/ComplaintsImage.js
+++ models/ComplaintsImage.js
@@ -8,15 +8,24 @@ import PartTimer from "./PartTimer.js";
 
 const ComplaintsImage = sequelize.define('ComplaintsImage', {
 
-    cno: {
+    cino: {
         type: DataTypes.BIGINT,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
     },
+    cfilename: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    cno: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+    }
+
 }, {
-    tableName: 'tbl_complaints',  // 실제 테이블 이름을 지정
+    tableName: 'tbl_complaintsImage',  // 실제 테이블 이름을 지정
     timestamps: false            // Sequelize가 자동으로 생성하는 createdAt, updatedAt을 사용하지 않으면 false로 설정
 })
 
-export default Complaints;
+export default ComplaintsImage;
