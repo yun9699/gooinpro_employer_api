0ea11f4e235b70d7fe442a5ccc3bdb7c2d22bbc5
yun9699 <cha23215@daum.net>
2025-01-20 15:54:12 +0900

    chore: db test

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
+
+// 데이터베이스 연결 모듈
+require('./config/dbConnection');
+
+// Morgan 로그 출력
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Hello, Node.js!');
