c9ec9d1ab7dae807a04a6ff96f835daa12910e28
jhyeong <hyeongcode@gmail.com>
2025-01-20 17:28:43 +0900

    feat: models/Review.js add

---
+++ models/Review.js
@@ -0,0 +1,36 @@
+import {sequelize} from "../config/db.js";
+import {DataTypes} from "sequelize";
+
+const Review = sequelize.define('Review', {
+
+    rno: {  //Primary Key
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    rstart: {   //별점(기본 별점은 5점으로)
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 5
+    },
+    rcontent: { //내용
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    rregdate: { //등록 시간
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    rdelete: {  //softDelete Flag
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+}, {
+    tableName: 'tbl_review',  // 실제 테이블 이름을 지정
+    timestamps: false            // Sequelize가 자동으로 생성하는 createdAt, updatedAt을 사용하지 않으면 false로 설정
+});
+
+export default Review;
--- models/index.js
+++ models/index.js
@@ -1,18 +1,25 @@
 import Employer from './Employer.js';
 import PartTimer from './PartTimer.js';
 import WorkLogs from './WorkLogs.js';
+import Review from "./Review.js";
 
 // 관계 정의
 
 //Employer
 Employer.hasMany(WorkLogs, { foreignKey: 'eno' });
+Employer.hasMany(Review, { foreignKey: 'eno' });
 
 //PartTImer
 PartTimer.hasMany(WorkLogs, { foreignKey: 'pno' });
+PartTimer.hasMany(Review, { foreignKey: 'pno' });
 
 //WorkLogs
 WorkLogs.belongsTo(Employer, { foreignKey: 'eno' });
 WorkLogs.belongsTo(PartTimer, { foreignKey: 'pno' });
+
+//Review
+Review.belongsTo(Employer, { foreignKey: 'eno' });
+Review.belongsTo(PartTimer, { foreignKey: 'pno' });
 
 
 const models = {
