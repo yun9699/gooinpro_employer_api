4eb24e2e7de23a8521f755c4e515b1164a305203
jhyeong <hyeongcode@gmail.com>
2025-03-13 18:14:12 +0900

    clore: Applicant List Pno RollBack

--- src/dto/partTimerdto/ApplicantListDTO.js
+++ src/dto/partTimerdto/ApplicantListDTO.js
@@ -1,13 +1,14 @@
 
 class ApplicantListDTO {
 
-    constructor(jpano, pifilename, pname, jpname, jpahourlyRate) {
+    constructor(jpano, pifilename, pname, jpname, jpahourlyRate, pno) {
 
         this.jpano = jpano;
         this.pifilename = pifilename;
         this.pname = pname;
         this.jpname = jpname;
         this.jpahourlyRate = jpahourlyRate;
+        this.pno = pno;
     }
 }
 
